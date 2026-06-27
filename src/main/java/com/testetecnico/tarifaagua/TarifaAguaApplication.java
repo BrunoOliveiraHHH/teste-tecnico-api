f6f5b32209e1e68faeb33d6cf64ee34436298ee7
Bruno Oliveira <bruno.d.l.oliveira@Outlook.com>
2026-06-27 01:21:22 -0300

    chore: estrutura inicial do projeto (Spring Boot 4, Maven, PostgreSQL)

---
+++ src/main/java/com/testetecnico/tarifaagua/TarifaAguaApplication.java
@@ -0,0 +1,12 @@
+package com.testetecnico.tarifaagua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarifaAguaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TarifaAguaApplication.class, args);
+    }
+}
